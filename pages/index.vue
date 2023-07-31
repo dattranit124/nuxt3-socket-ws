3ba08dbaa8ce4be0acc202792ac7c839dd1b25b7
dattranit124 <tranhocdatpc@gmail.com>
2023-07-31 16:27:50 +0700

    Added my project

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        123456
+    </div>
+</template>
+<script setup>
+// Default
+// const socket = useSocket()
+
+// Custom
+// const io = useIO()
+
+const connected = ref(false)
+
+// onMounted(() => {
+//   socket.on('connect', () => {
+//     connected.value = socket.connected
+//   })
+
+//   socket.on('disconnect', () => {
+//     connected.value = socket.connected
+//   })
+// })
+</script>
+
+<template>
+  <div>Connected: {{ connected }}</div>
+</template>
